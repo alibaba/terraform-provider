2a87d84a7987e3ad4a40191ddd3eface18ae9fb4
厚泼 <demon.wy@alibaba-inc.com>
2017-01-06 17:19:37 +0800

    set router_id

--- alicloud/resource_alicloud_vroute_entry.go
+++ alicloud/resource_alicloud_vroute_entry.go
@@ -64,6 +64,7 @@ func resourceAliyunRouteEntryCreate(d *schema.ResourceData, meta interface{}) er
 	}
 	// route_table_id:router_id:destination_cidrblock:nexthop_type:nexthop_id
 	d.SetId(rtId + ":" + rId + ":" + cidr + ":" + nt + ":" + ni)
+	d.Set("router_id", rId)
 
 	if err := conn.WaitForAllRouteEntriesAvailable(rId, rtId, defaultTimeout); err != nil {
 		return fmt.Errorf("WaitFor route entry got error: %#v", err)
--- terraform/examples/alicloud-vpc-route-entry/ouputs.tf
+++ terraform/examples/alicloud-vpc-route-entry/ouputs.tf
@@ -1,11 +1,15 @@
 output "route_table_id" {
-  value = "${alicloud_security_group_rule.ssh.route_table_id}"
+  value = "${alicloud_route_entry.default.route_table_id}"
+}
+
+output "router_id" {
+  value = "${alicloud_route_entry.default.router_id}"
 }
 
 output "nexthop_type" {
-  value = "${alicloud_security_group_rule.ssh.nexthop_type}"
+  value = "${alicloud_route_entry.default.nexthop_type}"
 }
 
 output "nexthop_id" {
-  value = "${alicloud_security_group_rule.ssh.nexthop_id}"
+  value = "${alicloud_route_entry.default.nexthop_id}"
 }
