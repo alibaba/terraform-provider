b849fc3853c17dc0edcb2b526d24957384106145
厚泼 <demon.wy@alibaba-inc.com>
2017-03-01 21:30:49 +0800

    forbid modify database

--- terraform/examples/alicloud-rds/main.tf
+++ terraform/examples/alicloud-rds/main.tf
@@ -4,13 +4,10 @@ resource "alicloud_db_instance" "dc" {
 	engine_version = "${var.engine_version}"
 	db_instance_class = "${var.instance_class}"
 	db_instance_storage = "${var.storage}"
-	instance_charge_type = "Postpaid"
 	db_instance_net_type = "${var.net_type}"
 
 	master_user_name = "${var.user_name}"
 	master_user_password = "${var.password}"
-
-  security_ips = ["10.168.1.12", "100.69.7.112"]
 
 	db_mappings = [{
 	      db_name = "${var.database_name}"
