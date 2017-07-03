f1663a9f708128f57e343211eb8dba3884045b0a
He Guimin <guimin.hgm@alibaba-inc.com>
2017-07-03 20:13:25 +0800

    fix there is no output ip_address error at first running apply

--- alicloud/resource_alicloud_eip.go
+++ alicloud/resource_alicloud_eip.go
@@ -64,9 +64,14 @@ func resourceAliyunEipCreate(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
+	err = conn.WaitForEip(getRegion(d, meta), allocationID, ecs.EipStatusAvailable, 60)
+	if err != nil {
+		return fmt.Errorf("Error Waitting for EIP available: %#v", err)
+	}
+
 	d.SetId(allocationID)
 
-	return resourceAliyunEipRead(d, meta)
+	return resourceAliyunEipUpdate(d, meta)
 }
 
 func resourceAliyunEipRead(d *schema.ResourceData, meta interface{}) error {
@@ -82,10 +87,9 @@ func resourceAliyunEipRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	if eip.InstanceId != "" {
-		d.Set("instance", eip.InstanceId)
+		d.Set("instance_id", eip.InstanceId)
 	} else {
-		d.Set("instance", "")
-		return nil
+		d.Set("instance_id", "")
 	}
 
 	bandwidth, _ := strconv.Atoi(eip.Bandwidth)
@@ -103,7 +107,7 @@ func resourceAliyunEipUpdate(d *schema.ResourceData, meta interface{}) error {
 
 	d.Partial(true)
 
-	if d.HasChange("bandwidth") {
+	if d.HasChange("bandwidth") && !d.IsNewResource() {
 		err := conn.ModifyEipAddressAttribute(d.Id(), d.Get("bandwidth").(int))
 		if err != nil {
 			return err
@@ -114,7 +118,7 @@ func resourceAliyunEipUpdate(d *schema.ResourceData, meta interface{}) error {
 
 	d.Partial(false)
 
-	return nil
+	return resourceAliyunEipRead(d, meta)
 }
 
 func resourceAliyunEipDelete(d *schema.ResourceData, meta interface{}) error {
