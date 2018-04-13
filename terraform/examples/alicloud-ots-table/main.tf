df0a270fcd58fe2976f3a26c09998db8f851dfad
zzxwill <zzxwill@gmail.com>
2018-04-13 18:12:02 +0800

    revise resource alicloud_ots_table and add testcase

--- terraform/examples/alicloud-ots-table/main.tf
+++ terraform/examples/alicloud-ots-table/main.tf
@@ -4,14 +4,24 @@ provider "alicloud" {
 resource "alicloud_ots_table" "table" {
   provider = "alicloud"
   table_name = "${var.table_name}"
-  primary_key_1_name = "${var.primary_key_1_name}"
-  primary_key_1_type = "${var.primary_key_1_type}"
-  primary_key_2_name = "${var.primary_key_2_name}"
-  primary_key_2_type = "${var.primary_key_2_type}"
-  primary_key_3_name = "${var.primary_key_3_name}"
-  primary_key_3_type = "${var.primary_key_3_type}"
-  primary_key_4_name = "${var.primary_key_4_name}"
-  primary_key_4_type = "${var.primary_key_4_type}"
-  time_to_live = "${var.time_to_live}"
-  max_version = "${var.max_version}"
+  primary_key {
+    name = "${var.primary_key_1_name}"
+    type = "${var.primary_key_1_type}"
+  }
+  primary_key {
+    name = "${var.primary_key_2_name}"
+    type = "${var.primary_key_2_type}"
+  }
+  primary_key {
+    name = "${var.primary_key_3_name}"
+    type = "${var.primary_key_3_type}"
+  }
+  primary_key {
+    name = "${var.primary_key_4_name}"
+    type = "${var.primary_key_4_type}"
+  }
+  table_option {
+    time_to_live = "${var.time_to_live}"
+    max_version = "${var.max_version}"
+  }
 }
