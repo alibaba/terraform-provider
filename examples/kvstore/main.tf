37ec6bd9f9e64e7c7f45fa49664591f126a330d6
arafato <arafato@outlook.com>
2018-06-14 17:17:23 +0200

    updated

--- examples/kvstore/main.tf
+++ examples/kvstore/main.tf
@@ -1,3 +1,31 @@
-data "alicloud_rkv_instances" "rkv_instance" {
-  output_file = "out.dat"
+# data "alicloud_rkv_instances" "rkv_instance" {
+#   output_file = "out.dat"
+# }
+
+# data "alicloud_zones" "default" {
+#   available_resource_creation = "Rkv"
+# }
+
+# // VPC Resource for Module
+# resource "alicloud_vpc" "vpc" {
+#   count = "${var.vpc_id == "" ? 1 : 0}"
+
+#   name       = "${var.vpc_name}"
+#   cidr_block = "${var.vpc_cidr}"
+# }
+
+# // VSwitch Resource for Module
+# resource "alicloud_vswitch" "vswitch" {
+#   count             = "${var.vswitch_id == "" ? 1 : 0}"
+#   availability_zone = "${var.availability_zone == "" ? data.alicloud_zones.default.zones.0.id : var.availability_zone}"
+#   name              = "${var.vswitch_name}"
+#   cidr_block        = "${var.vswitch_cidr}"
+#   vpc_id            = "${var.vpc_id == "" ? alicloud_vpc.vpc.id : var.vpc_id}"
+# }
+
+resource "alicloud_rkv_instance" "myredis2" {
+  instance_class = "${var.instance_class}"
+  instance_name  = "${var.instance_name}"
+  password       = "${var.password}"
+  vswitch_id     = "${var.vswitch_id}"
 }
