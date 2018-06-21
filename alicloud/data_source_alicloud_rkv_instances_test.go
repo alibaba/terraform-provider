cbfd129e75ef2766f4097e5e4d5c3c565b4c1add
arafato <arafato@outlook.com>
2018-06-21 14:41:20 +0200

    added unit tests for rkv data source.

---
+++ alicloud/data_source_alicloud_rkv_instances_test.go
@@ -0,0 +1,45 @@
+package alicloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccAlicloudRKVInstancesDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAlicloudRKVInstancesDataSourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAlicloudDataSourceID("data.alicloud_rkv_instances.rkvs"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.#", "1"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.0.instance_class", "redis.master.small.default"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.0.name", "myredis"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.0.instance_type", "Redis"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.0.instance_type", "Redis"),
+					resource.TestCheckResourceAttr("data.alicloud_rkv_instances.rkvs", "instances.0.charge_type", string(Postpaid)),
+				),
+			},
+		},
+	})
+}
+
+const testAccCheckAlicloudRKVInstancesDataSourceConfig = `
+data "alicloud_rkv_instances" "rkvs" {
+  name_regex = "${alicloud_rkv_instance.rkv.instance_name}"
+}
+
+resource "alicloud_rkv_instance" "rkv" {
+	instance_name = "myredis"
+	instance_class = "redis.master.small.default"
+	instance_type = "Redis"
+	engine_version = "2.8"
+	password = "Passw0rd"
+    charge_type = "PostPaid"
+}
+`
