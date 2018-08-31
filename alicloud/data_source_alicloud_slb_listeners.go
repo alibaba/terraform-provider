5f34bf42f1586a73bcafe9b79f47da6aa95e920e
Marc Plouhinec <m_plouhinec@yahoo.fr>
2018-08-31 18:23:28 +0200

    Enrich the alicloud_slb_listeners data source with more details when the protocol is http.

--- alicloud/data_source_alicloud_slb_listeners.go
+++ alicloud/data_source_alicloud_slb_listeners.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 )
 
 func dataSourceAlicloudSlbListeners() *schema.Resource {
@@ -28,8 +29,6 @@ func dataSourceAlicloudSlbListeners() *schema.Resource {
 				ForceNew: true,
 			},
 
-			// TODO
-
 			// Computed values
 			"slb_listeners": {
 				Type:     schema.TypeList,
@@ -40,11 +39,102 @@ func dataSourceAlicloudSlbListeners() *schema.Resource {
 							Type:     schema.TypeInt,
 							Computed: true,
 						},
+						"backend_port": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
 						"protocol": {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
-						// TODO
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"bandwidth": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"scheduler": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"sticky_session": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"sticky_session_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cookie_timeout": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"cookie": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"health_check": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"health_check_domain": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"health_check_uri": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"health_check_connect_port": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"healthy_threshold": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"unhealthy_threshold": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"health_check_timeout": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"health_check_interval": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"health_check_http_code": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"gzip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"x_forwarded_for": {
+							Type:     schema.TypeMap,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"retrieve_slb_ip": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+									"retrieve_slb_id": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+									"retrieve_slb_proto": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+								},
+							},
+						},
 					},
 				},
 			},
@@ -100,6 +190,8 @@ func dataSourceAlicloudSlbListenersRead(d *schema.ResourceData, meta interface{}
 }
 
 func slbListenersDescriptionAttributes(d *schema.ResourceData, listeners []slb.ListenerPortAndProtocol, meta interface{}) error {
+	conn := meta.(*AliyunClient).slbconn
+
 	var ids []string
 	var s []map[string]interface{}
 
@@ -109,7 +201,50 @@ func slbListenersDescriptionAttributes(d *schema.ResourceData, listeners []slb.L
 			"protocol":      listener.ListenerProtocol,
 		}
 
-		// TODO get more info
+		loadBalancerId := d.Get("load_balancer_id").(string)
+		switch Protocol(listener.ListenerProtocol) {
+		case Http:
+			args := slb.CreateDescribeLoadBalancerHTTPListenerAttributeRequest()
+			args.LoadBalancerId = loadBalancerId
+			args.ListenerPort = requests.NewInteger(listener.ListenerPort)
+			resp, err := conn.DescribeLoadBalancerHTTPListenerAttribute(args)
+			if err == nil {
+				mapping["backend_port"] = resp.BackendServerPort
+				mapping["status"] = resp.Status
+				mapping["bandwidth"] = resp.Bandwidth
+				mapping["scheduler"] = resp.Scheduler
+				mapping["sticky_session"] = resp.StickySession
+				mapping["sticky_session_type"] = resp.StickySessionType
+				mapping["cookie_timeout"] = resp.CookieTimeout
+				mapping["cookie"] = resp.Cookie
+				mapping["health_check"] = resp.HealthCheck
+				mapping["health_check_domain"] = resp.HealthCheckDomain
+				mapping["health_check_uri"] = resp.HealthCheckURI
+				mapping["health_check_connect_port"] = resp.HealthCheckConnectPort
+				mapping["healthy_threshold"] = resp.HealthyThreshold
+				mapping["unhealthy_threshold"] = resp.UnhealthyThreshold
+				mapping["health_check_timeout"] = resp.HealthCheckTimeout
+				mapping["health_check_interval"] = resp.HealthCheckInterval
+				mapping["health_check_http_code"] = resp.HealthCheckHttpCode
+				mapping["gzip"] = resp.Gzip
+				if resp.XForwardedFor == string(OnFlag) {
+					mapping["x_forwarded_for"] = map[string]interface{}{
+						"retrieve_slb_ip":    resp.XForwardedForSLBIP,
+						"retrieve_slb_id":    resp.XForwardedForSLBID,
+						"retrieve_slb_proto": resp.XForwardedForProto,
+					}
+				}
+			} else {
+				log.Printf("[WARN] alicloud_slb_listeners - DescribeLoadBalancerHTTPListenerAttribute error: %v", err)
+			}
+			// TODO
+		case Https:
+			// TODO
+		case Tcp:
+			// TODO
+		case Udp:
+			// TODO
+		}
 
 		log.Printf("[DEBUG] alicloud_slb_listeners - adding slb_listener mapping: %v", mapping)
 		ids = append(ids, strconv.Itoa(listener.ListenerPort))
