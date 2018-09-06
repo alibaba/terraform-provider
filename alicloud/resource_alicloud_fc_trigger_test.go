0972902d00c56d61e5e8de4372726936d84834d8
Marc Plouhinec <m_plouhinec@yahoo.fr>
2018-09-06 11:54:05 +0200

    Use unique bucket names in order to allow 2 users to run the same tests at the same time on the same region.

--- alicloud/resource_alicloud_fc_trigger_test.go
+++ alicloud/resource_alicloud_fc_trigger_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aliyun/aliyun-log-go-sdk"
 	"github.com/aliyun/fc-go-sdk"
+	"github.com/hashicorp/terraform/helper/acctest"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -24,28 +25,29 @@ func TestAccAlicloudFCTrigger_log(t *testing.T) {
 	var function fc.GetFunctionOutput
 	var trigger fc.GetTriggerOutput
 
+	randInt := acctest.RandInt()
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAlicloudFCTriggerDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAlicloudFCTriggerLog(testTriggerLogTemplate, testFCLogRoleTemplate, testFCLogPolicyTemplate),
+				Config: testAlicloudFCTriggerLog(testTriggerLogTemplate, testFCLogRoleTemplate, testFCLogPolicyTemplate, randInt),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudLogProjectExists("alicloud_log_project.foo", &project),
 					testAccCheckAlicloudLogStoreExists("alicloud_log_store.foo", &store),
 					testAccCheckAlicloudFCServiceExists("alicloud_fc_service.foo", &service),
 					testAccCheckAlicloudFCFunctionExists("alicloud_fc_function.foo", &function),
 					testAccCheckAlicloudFCTriggerExists("alicloud_fc_trigger.foo", &trigger),
-					resource.TestCheckResourceAttr("alicloud_fc_trigger.foo", "name", "test-alicloud-fc-trigger"),
+					resource.TestCheckResourceAttr("alicloud_fc_trigger.foo", "name", fmt.Sprintf("test-alicloud-fc-trigger-%v", randInt)),
 					resource.TestCheckResourceAttrSet("alicloud_fc_trigger.foo", "config"),
 				),
 			},
 			{
-				Config: testAlicloudFCTriggerLogUpdate(testTriggerLogTemplateUpdate, testFCLogRoleTemplate, testFCLogPolicyTemplate),
+				Config: testAlicloudFCTriggerLogUpdate(testTriggerLogTemplateUpdate, testFCLogRoleTemplate, testFCLogPolicyTemplate, randInt),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAlicloudFCTriggerExists("alicloud_fc_trigger.foo", &trigger),
-					resource.TestCheckResourceAttr("alicloud_fc_trigger.foo", "name", "test-alicloud-fc-trigger"),
+					resource.TestCheckResourceAttr("alicloud_fc_trigger.foo", "name", fmt.Sprintf("test-alicloud-fc-trigger-%v", randInt)),
 					resource.TestCheckResourceAttrSet("alicloud_fc_trigger.foo", "config"),
 				),
 			},
@@ -103,7 +105,7 @@ func testAccCheckAlicloudFCTriggerDestroy(s *terraform.State) error {
 	return nil
 }
 
-func testAlicloudFCTriggerLog(trigger, role, policy string) string {
+func testAlicloudFCTriggerLog(trigger, role, policy string, randInt int) string {
 	return fmt.Sprintf(`
 provider "alicloud" {
   account_id = "${var.account}"
@@ -116,7 +118,7 @@ variable "account" {
   default = "1204663572767468"
 }
 variable "name" {
-  default = "test-alicloud-fc-trigger"
+  default = "test-alicloud-fc-trigger-%v"
 }
 
 resource "alicloud_log_project" "foo" {
@@ -201,10 +203,10 @@ resource "alicloud_ram_role_policy_attachment" "foo" {
   policy_name = "${alicloud_ram_policy.foo.name}"
   policy_type = "Custom"
 }
-`, trigger, role, policy)
+`, randInt, trigger, role, policy)
 }
 
-func testAlicloudFCTriggerLogUpdate(trigger, role, policy string) string {
+func testAlicloudFCTriggerLogUpdate(trigger, role, policy string, randInt int) string {
 	return fmt.Sprintf(`
 provider "alicloud" {
   account_id = "${var.account}"
@@ -217,7 +219,7 @@ variable "account" {
   default = "1204663572767468"
 }
 variable "name" {
-  default = "test-alicloud-fc-trigger"
+  default = "test-alicloud-fc-trigger-%v"
 }
 
 resource "alicloud_log_project" "foo" {
@@ -302,7 +304,7 @@ resource "alicloud_ram_role_policy_attachment" "foo" {
   policy_name = "${alicloud_ram_policy.foo.name}"
   policy_type = "Custom"
 }
-`, trigger, role, policy)
+`, randInt, trigger, role, policy)
 }
 
 var testTriggerLogTemplate = `
